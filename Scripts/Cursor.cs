3737d1d5bcc9a0198d37f39e956f3a550686ea5b
Gabriel Furlan Mengarelli <gabriel@mengarelli.com.br>
2024-12-07 14:18:58 -0300

    Cursor invisivel

--- Scripts/Cursor.cs
+++ Scripts/Cursor.cs
@@ -9,13 +9,11 @@ public class Cursor : MonoBehaviour
     float cameraVerticalRotation = 0f;
 
 
-    //bool lockedCursor = true;
-
     void Start()
     {
         //Lock and Hide Cursor
-        /*Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;*/
+        UnityEngine.Cursor.visible = false;
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -24,17 +22,13 @@ public class Cursor : MonoBehaviour
         //Collect Mouse Input
         float inputX = Input.GetAxis("Mouse X") * mouseSensivity;
         float inputY = Input.GetAxis("Mouse Y") * mouseSensivity;
-        /*
-        if (Input.GetKey(KeyCode.V))
-        {
-            player.Rotate(Vector3.left * mouseSensivity);
-        }
-        */
+
         //Rotate the Camera around its local X axis
         cameraVerticalRotation -= inputY;
         cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
         transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
 
+        //Rotate the Camera around its Y axis
         player.Rotate(Vector3.up * inputX);
     }
 }
