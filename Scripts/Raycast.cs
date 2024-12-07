d43611fbdeaf52b2ca13c41c7e81f85295f08e02
ArthurPolakFurman <arthur.polak.furman@gmail.com>
2024-12-07 15:11:58 -0300

    Raycast funcionando com tags
    Se estiver olhando pra objeto com tag Floor spawna uma bola, se olhar pro um objeto com tag de Cube aumenta a escala do objeto que o raycast acertou (o cubo), no futuro adicionar pra sumir o objeto e colocar no inventário.

---
+++ Scripts/Raycast.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public Transform cam;
+    public float PlayerActivateDistance;
+    bool active = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        active = Physics.Raycast(cam.position, cam.TransformDirection(Vector3.forward), out hit, PlayerActivateDistance);
+        Debug.DrawRay(cam.position, cam.TransformDirection(Vector3.forward) * PlayerActivateDistance, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.F) && active)
+        {
+            // Check the tag of the object hit
+            if (hit.collider.CompareTag("Floor"))
+            {
+                // Spawn a sphere at the hit point
+                GameObject ball = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                ball.AddComponent<Rigidbody>();
+                ball.transform.position = hit.point; // Place the ball at the raycast hit location
+                ball.transform.position += new Vector3(0, 1, 0);
+            }
+            else if (hit.collider.CompareTag("Cube"))
+            {
+                // Scale the cube
+                Transform cubeTransform = hit.collider.transform;
+                cubeTransform.localScale += Vector3.one; 
+            }
+        }
+    }
+}
