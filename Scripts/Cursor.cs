2735b6fa991be4135c7093a50fc4ec4bc5322096
ArthurPolakFurman <arthur.polak.furman@gmail.com>
2024-12-07 01:53:46 -0300

    Concertando a merda toda
    plum plum

--- Scripts/Cursor.cs
+++ Scripts/Cursor.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Cursor : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Variables
+    public Transform player;
+    public float mouseSensivity = 2f;
+    float cameraVerticalRotation = 0f;
+
+    //bool lockedCursor = true;
+
     void Start()
     {
-        
+        //Lock and Hide Cursor
+        /*Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;*/
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Collect Mouse Input
+        float inputX = Input.GetAxis("Mouse X") * mouseSensivity;
+        float inputY = Input.GetAxis("Mouse Y") * mouseSensivity;
+
+        //Rotate the Camera around its local X axis
+        cameraVerticalRotation -= inputY;
+        cameraVerticalRotation = Mathf.Clamp(cameraVerticalRotation, -90f, 90f);
+        transform.localEulerAngles = Vector3.right * cameraVerticalRotation;
+
+        //Rotate the Player Object and the Camera around its Y axis
+        player.Rotate(Vector3.up * inputX);
     }
 }
